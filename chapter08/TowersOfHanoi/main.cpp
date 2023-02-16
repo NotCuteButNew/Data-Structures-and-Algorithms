51bcb417672243a5fb733bf181faf3fcd4fc783d
NotCuteButNew <2386884119@qq.com>
2023-02-16 18:37:55 +0800

    编写完成，并成功运行

---
+++ chapter08/TowersOfHanoi/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <ostream>
+#include <string>
+
+void TowersOfHanoi(int n, char source, char target, char transit) {
+  if (n > 0) {
+    TowersOfHanoi(n - 1, source, transit, target);
+    std::cout << source << "\t->\t" << target << std::endl;
+    TowersOfHanoi(n - 1, transit, source, target);
+  }
+}
+
+int main(int argc, char **argv) {
+  if (argc > 1) {
+    int n = std::stoi(argv[1]);
+    TowersOfHanoi(n, 'A', 'B', 'C');
+  }
+  return 0;
+}
